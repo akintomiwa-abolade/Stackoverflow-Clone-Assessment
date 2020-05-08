c4bb2294b7bad8c84a7a526c08b8523b94025b3a
JBoss <abolade.akintomiwa@gmail.com>
2020-05-08 10:59:53 +0100

    User routes handled

--- routes/user.js
+++ routes/user.js
@@ -14,147 +14,15 @@ const secret = process.env.SECRET;
 const UserController = require('../controllers/UserController');
 
 // register user
-router.post('/users/register', function(req, res){
-	try{
-
-		let { name, email, password, fcm_token} = req.body;
-		req.checkBody('name', 'The Name Field is required').notEmpty();
-		req.checkBody('email', 'The Email Field is required').notEmpty();
-		req.checkBody('email', 'The Email is not valid').isEmail();
-		req.checkBody('password', 'The Password Field is required').notEmpty();
-		req.checkBody('fcm_token', 'The fcm_token Field is required').notEmpty();
-
-		let errors = req.validationErrors();
-		if(errors){
-			return res.status(200).json({
-				error:true,
-				message:errors
-			});
-		}
-
-	    let hashedPassword = bcrypt.hashSync(password, 10);
-	    
-	    let newUser = {
-	      name, email, password: hashedPassword, fcm_token
-	    }
-
-	    User.create(newUser)
-		.then(data => {
-			return res.status(201).json(data);
-		})
-		.catch(err => { return res.status(500).json({message: err.message}); })
-	}catch(e){
-		return res.status(500).json({message:e.message});
-	}
-});
-
+router.post('/users/register', UserController.registerUser);
 // user login
-router.post('/users/login', function(req, res){
-	try{
-		let { email, password } = req.body;
-
-		// validate entries
-		req.checkBody('email', 'The Email Field is required').notEmpty();
-		req.checkBody('email', 'The Email is not valid').isEmail();
-		req.checkBody('password', 'The Password Field is required').notEmpty();
-
-		let errors = req.validationErrors();
-		if(errors){
-			return res.status(200).json({
-				error:true,
-				message:errors
-			});
-		}
-
-	    User.findOne({ email: email })
-		.then(user => {
-			if (!user) {
-			  res.status(401).json({error:true,message: "sorry no user found"});
-			} else {
-				var passwordIsValid = bcrypt.compareSync(password, user.password);
-				if (!passwordIsValid) {
-					return res.status(401).json({
-					  error: true,
-					  message: 'Authentication failed.'
-					});
-				} else {
-
-					let obj = {
-					  id: user._id,
-					  email: user.email,
-					  fcm_token:user.email
-					}
-
-					let token = jwt.sign({
-					  user: obj
-					}, secret, {
-					  expiresIn: '1d'
-					});
-
-					return res.status(200).json({
-					  token,
-					  email,
-					  name: user.name
-					});
-				}
-			}
-		})
-		.catch(err => { res.status(500).json({message: err.message}) })
-
-	}catch(e){
-		return res.status(500).json({message:e.message});
-	}
-});
-
+router.post('/users/login', UserController.userLogin);
 // fetch all users
-router.get('/users', function(req, res){
-	User.find()
-	.then(data => {
-	res.status(200).json(data)
-	})
-	.catch(err =>  res.status(500).json({message: err.message}) );
-});
-
+router.get('/users', UserController.fetchUsers);
 // get specific user info
-router.get('/users/:userId', function(req, res){
-	try{
-
-		User.findOne({_id: req.params.userId})
-		.then(user => {
-			res.status(200).json(user)
-		})
-		.catch(err => res.status(500).json({message: err.message}) );
-	}catch(e){
-		return res.status(500).json({message:e.message});
-	}
-});
-
+router.get('/users/:userId', UserController.fetchSingleUser);
 // search for user
-router.post('/user/search', function(req, res){
-	try{
-
-		let { search } = req.body;
-
-		// validate entry
-		req.checkBody('search', 'The Search Field is required').notEmpty();
-
-		let errors = req.validationErrors();
-		if(errors){
-			return res.status(200).json({
-				error:true,
-				message:errors
-			});
-		}
-
-		User.find({name:search})
-		.then(data => {
-			res.status(200).json(data)
-		})
-		.catch(err =>  res.status(500).json({message: err.message}) );
-	}catch(e){
-		return res.status(500).json({message:e.message});
-	}
-});
+router.post('/user/search', UserController.searchExistingUser);
 
 
 module.exports = router;
