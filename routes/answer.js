75bd92aae4c4630b282bfd6df10fc3257a161ba7
JBoss <abolade.akintomiwa@gmail.com>
2020-05-09 13:00:43 +0100

    All model routes are handled here

--- routes/answer.js
+++ routes/answer.js
@@ -8,11 +8,75 @@ const router = express.Router();
 const { authenticate, authorize } = require('../middlewares/auth');
 const AnswerController = require('../controllers/AnswerController');
 
-// view specific question answers
+/**
+ * @swagger
+ * /answers/question/{questionId}:
+ *   get:
+ *     tags:
+ *       - Answers
+ *     name: List question answers
+ *     summary: List all answers for specific question asked
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required:
+ *           - questionId
+ *     responses:
+ *       200:
+ *         description: Single question answers object
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/answers/question/:questionId', AnswerController.fetchSpecificQuestionAnswer);
 // get all question answered by loggedin user
 router.get('/answers/user', authenticate, AnswerController.fetchAnswersByUser);
-// answer question
+/**
+ * @swagger
+ * /answers:
+ *   post:
+ *     tags:
+ *       - Answers
+ *     name: Ask Question
+ *     summary: Ask new question
+ *     security:
+ *       - bearerAuth: []
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           $ref: '#/definitions/Answer'
+ *           type: object
+ *           properties:
+ *             title:
+ *               type: string
+ *             description:
+ *               type: string
+ *             question:
+ *               type: string
+ *         required:
+ *           - title
+ *           - decription
+ *           - question
+ *     responses:
+ *       '200':
+ *         description: Validation error message
+ *       '201':
+ *         description: New answer created successfully for question
+ *       '403':
+ *         description: No auth token
+ *       '500':
+ *         description: Internal server error
+ */
 router.post('/answers', authenticate, AnswerController.answerQuestion);
 // get specific answer
 router.get('/answers/:answerId', AnswerController.fetchAnswer);
@@ -20,7 +84,34 @@ router.get('/answers/:answerId', AnswerController.fetchAnswer);
 router.put('/answers/:answerId/upvote', authenticate, AnswerController.upVoteAnswer);
 // downvote answer
 router.put('/answers/:answerId/downvote', authenticate, AnswerController.downVoteAnswer);
-// search for answer
+/**
+ * @swagger
+ * /answers/search:
+ *   post:
+ *     tags:
+ *       - Answers
+ *     name: Search Answers
+ *     summary: Search answers by title
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             search:
+ *               type: string
+ *         required:
+ *           - search
+ *     responses:
+ *       '200':
+ *         description: search answer result array
+ *       '500':
+ *         description: Internal server error
+ */
 router.post('/answers/search', AnswerController.searchAnswer);
 // update answer
 router.put('/answers/:answerId', authenticate, authorize, AnswerController.updateAnswer);
--- routes/question.js
+++ routes/question.js
@@ -6,62 +6,223 @@
 
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
 const { authenticate, authorize } = require('../middlewares/auth');
-const notificationServer = require('../middlewares/notificationServer');
 
 const QuestionController = require('../controllers/QuestionController');
 
-// view all question
+/**
+ * @swagger
+ * /questions:
+ *   get:
+ *     tags:
+ *       - Questions
+ *     name: List questions
+ *     summary: List all questions asked
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: All questions object
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/questions', QuestionController.fetchQuestions);
 /**
  * @swagger
  * /questions:
  *   post:
  *     tags:
- *       - Users
- *     name: AskQuestion
- *     summary: User Ask Question
- *     consumes:
- *       - application/json
+ *       - Questions
+ *     name: Ask Question
+ *     summary: Ask new question
+ *     security:
+ *       - bearerAuth: []
  *     produces:
+ *       - application/json
+ *     consumes:
  *       - application/json
  *     parameters:
  *       - name: body
  *         in: body
  *         schema:
- *           $ref: '#/definitions/User'
+ *           $ref: '#/definitions/Question'
  *           type: object
  *           properties:
  *             title:
  *               type: string
  *             description:
  *               type: string
- *             user:
- *               type: integer
- *             upvote:
- *               type: ObjectId
  *         required:
  *           - title
  *           - description
  *     responses:
  *       '201':
- *         description: Question asked successfully
+ *         description: New question created successfully
+ *       '403':
+ *         description: No token provided
+ *       '500':
+ *         description: Internal server error
  */
 router.post('/questions', authenticate, QuestionController.askQuestion);
 // view all user questions
 router.get('/questions/user', authenticate, QuestionController.fetchUsersQuestions);
-// get single question
+/**
+ * @swagger
+ * /questions/{questionId}:
+ *   get:
+ *     tags:
+ *       - Questions
+ *     name: Fetch Single Question
+ *     summary: Display user Single Question
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required:
+ *           - questionId
+ *     responses:
+ *       200:
+ *         description: Single question object
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/questions/:questionId', QuestionController.fetchSingleQuestion);
+/**
+ * @swagger
+ * /questions/subscribe:
+ *   post:
+ *     tags:
+ *       - Questions
+ *     name: Subscribe to Question
+ *     summary: A user subscribe to question
+ *     security:
+ *       - bearerAuth: []
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             questionId:
+ *               type: string
+ *         required:
+ *           - questionId
+ *     responses:
+ *       '200':
+ *         description: Subscription Failed with error message
+ *       '201':
+ *         description: Subscription Successful
+ *       '403':
+ *         description: No auth token
+ *       '500':
+ *         description: Internal server error
+ */
 router.post('/questions/subscribe', authenticate, QuestionController.userSubscribeToQuestion)
-// give question a up vote
+/**
+ * @swagger
+ * /questions/{questionId}/upvote:
+ *   put:
+ *     tags:
+ *       - Questions
+ *     name: List questions
+ *     summary: List all questions asked
+ *     security:
+ *       - bearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required:
+ *           - questionId
+ *     responses:
+ *       200:
+ *         description: upvote successful and question object
+ *       '403':
+ *         description: No auth token
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/questions/:questionId/upvote', authenticate, QuestionController.upVoteQuestion);
-// give question a down vote
+/**
+ * @swagger
+ * /questions/{questionId}/downvote:
+ *   put:
+ *     tags:
+ *       - Questions
+ *     name: List questions
+ *     summary: List all questions asked
+ *     security:
+ *       - bearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required:
+ *           - questionId
+ *     responses:
+ *       200:
+ *         description: downvote successful and question object
+ *       '403':
+ *         description: No auth token
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/questions/:questionId/downvote', authenticate, QuestionController.downVoteQuestion);
 // update question
 router.put('/questions/:questionId', authenticate, authorize, QuestionController.updateQuestion)
 // delete question
 router.delete('/questions/:questionId', authenticate, authorize, QuestionController.deleteQuestion)
-// search for question
+/**
+ * @swagger
+ * /questions/search:
+ *   post:
+ *     tags:
+ *       - Questions
+ *     name: Search questions
+ *     summary: Search questions by title
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             search:
+ *               type: string
+ *         required:
+ *           - search
+ *     responses:
+ *       '200':
+ *         description: search question result array
+ *       '500':
+ *         description: Internal server error
+ */
 router.post('/questions/search', QuestionController.searchQuestion);
 
 module.exports = router;
